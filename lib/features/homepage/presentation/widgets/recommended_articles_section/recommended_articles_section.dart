4b54b872c2bf0ab311f18a53503216d7de026007
anshra1 <020ansh@gmail.com>
2026-01-28 15:30:09 +0530

    feat(homepage): add recommended articles section

---
+++ lib/features/homepage/presentation/widgets/recommended_articles_section/recommended_articles_section.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RecommendedArticlesSection extends StatelessWidget {
+  const RecommendedArticlesSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Recommended Articles Section'),
+    );
+  }
+}
